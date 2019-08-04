a4223062c8840a13b267e7da8147d66a66310590
odonfrancisco <francisco.odon11@gmail.com>
2019-08-04 00:48:19 -0400

    added layout for uber clone as well as on click listener for the switch (took me longer than expected but it's cool i'm learning)

--- uberClone/app/src/main/java/c/odonfrancisco/uberclone/MainActivity.java
+++ uberClone/app/src/main/java/c/odonfrancisco/uberclone/MainActivity.java
@@ -3,13 +3,30 @@ package c.odonfrancisco.uberclone;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Switch;
+import android.widget.Toast;
 
 import com.parse.ParseAnalytics;
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.SaveCallback;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.driverRiderSwitch){
+            Switch driverSwitch = (Switch) v;
+            if(driverSwitch.isChecked()){
+                Log.i("switch checked", "true");
+                Toast.makeText(getApplicationContext(), "True", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(getApplicationContext(), "False", Toast.LENGTH_SHORT).show();
+                Log.i("switched checked", "false");
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +35,8 @@ public class MainActivity extends AppCompatActivity {
 
         ParseObject exampleObject = new ParseObject("ExampleObject");
         exampleObject.put("working", "true");
+
+        findViewById(R.id.driverRiderSwitch).setOnClickListener(this);
 
         exampleObject.saveInBackground(new SaveCallback() {
             @Override
@@ -32,4 +51,5 @@ public class MainActivity extends AppCompatActivity {
 
         ParseAnalytics.trackAppOpenedInBackground(getIntent());
     }
+
 }
