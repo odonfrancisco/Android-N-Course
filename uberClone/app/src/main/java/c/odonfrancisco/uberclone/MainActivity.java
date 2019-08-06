586a52577a8c5abc683e4690017def83c1b9279a
odonfrancisco <francisco.odon11@gmail.com>
2019-08-05 23:29:05 -0400

    Anonymous login finished for riders and drivers. Now going to work on Rider Activity

--- uberClone/app/src/main/java/c/odonfrancisco/uberclone/MainActivity.java
+++ uberClone/app/src/main/java/c/odonfrancisco/uberclone/MainActivity.java
@@ -7,23 +7,35 @@ import android.view.View;
 import android.widget.Switch;
 import android.widget.Toast;
 
+import com.parse.LogInCallback;
+import com.parse.Parse;
 import com.parse.ParseAnalytics;
+import com.parse.ParseAnonymousUtils;
 import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseUser;
 import com.parse.SaveCallback;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private boolean driver;
 
     @Override
     public void onClick(View v) {
-        if(v.getId() == R.id.driverRiderSwitch){
-            Switch driverSwitch = (Switch) v;
-            if(driverSwitch.isChecked()){
-                Log.i("switch checked", "true");
-                Toast.makeText(getApplicationContext(), "True", Toast.LENGTH_SHORT).show();
-            } else {
-                Toast.makeText(getApplicationContext(), "False", Toast.LENGTH_SHORT).show();
-                Log.i("switched checked", "false");
+        switch(v.getId()){
+            case R.id.driverRiderSwitch: {
+                Switch driverSwitch = (Switch) v;
+                if(driverSwitch.isChecked()){
+                    driver = true;
+                    Log.i("switch checked", "true");
+                    Toast.makeText(getApplicationContext(), "True", Toast.LENGTH_SHORT).show();
+                } else {
+                    driver = false;
+                    Toast.makeText(getApplicationContext(), "False", Toast.LENGTH_SHORT).show();
+                    Log.i("switched checked", "false");
+                }
+            } break;
+            case R.id.startButton : {
+                anonymousLogin();
             }
         }
     }
@@ -32,11 +44,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        driver = false;
 
         ParseObject exampleObject = new ParseObject("ExampleObject");
         exampleObject.put("working", "true");
 
         findViewById(R.id.driverRiderSwitch).setOnClickListener(this);
+        findViewById(R.id.startButton).setOnClickListener(this);
 
         exampleObject.saveInBackground(new SaveCallback() {
             @Override
@@ -50,6 +66,38 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         });
 
         ParseAnalytics.trackAppOpenedInBackground(getIntent());
+    }
+
+    private void getStarted(){
+        if((boolean) ParseUser.getCurrentUser().get("driver")){
+            Log.i("Redirecting as", "Driver");
+        } else {
+            Log.i("Redirecting as", "Rider");
+        }
+
+    }
+
+    private boolean anonymousLogin(){
+        if(ParseUser.getCurrentUser().get("driver") == null){
+            ParseAnonymousUtils.logIn(new LogInCallback() {
+                @Override
+                public void done(ParseUser user, ParseException e) {
+                    if (e == null){
+                        if(driver){
+                            Log.i("Login", "Successful as driver");
+                        } else {
+                            Log.i("Login", "Successful as rider");
+                        }
+                        user.put("driver", driver);
+                        getStarted();
+                    }
+                }
+            });
+        } else {
+            ParseUser.getCurrentUser().put("driver", driver);
+            getStarted();
+        }
+        return true;
     }
 
 }
