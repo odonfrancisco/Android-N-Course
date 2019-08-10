879e0dbb951cafcf1104e6532e1a2314cf524770
odonfrancisco <francisco.odon11@gmail.com>
2019-08-10 16:46:04 -0400

    Driver now able to view list of distances. When clicks on one, takes driver to map activity. Am passing rider location to new map activity. Need to work on displaying the two locations on the same map with markers and corresponding zoom

--- uberClone/app/src/main/java/c/odonfrancisco/uberclone/DriverActivity.java
+++ uberClone/app/src/main/java/c/odonfrancisco/uberclone/DriverActivity.java
@@ -6,11 +6,14 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
@@ -19,6 +22,7 @@ import com.google.android.gms.maps.model.LatLng;
 import com.parse.FindCallback;
 import com.parse.Parse;
 import com.parse.ParseException;
+import com.parse.ParseGeoPoint;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 
@@ -38,9 +42,36 @@ public class DriverActivity extends AppCompatActivity {
         setContentView(R.layout.activity_driver);
 
         requestListView = findViewById(R.id.requestsListView);
+
+        requestListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(), DriverMapActivity.class);
+                ParseGeoPoint riderRequest = (ParseGeoPoint) ((ParseObject) requestsList.get(position)).get("riderLocation");
+                intent.putExtra("riderLat", String.valueOf(riderRequest.getLatitude()));
+                intent.putExtra("riderLong", String.valueOf(riderRequest.getLongitude()));
+
+                startActivity(intent);
+            }
+        });
+
+        getRequests();
         setLocationListener();
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(requestCode == 1){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10000, 120, locationListener);
+                }
+            }
+        }
+
+    }
 
     private void setLocationListener(){
         locationManager = (LocationManager) this.getSystemService(LOCATION_SERVICE);
@@ -50,7 +81,7 @@ public class DriverActivity extends AppCompatActivity {
             public void onLocationChanged(Location location) {
                 Log.i("locationToString", location.toString());
                 userLocation = location;
-                getRequests();
+                setListView();
             }
 
             @Override
@@ -77,11 +108,13 @@ public class DriverActivity extends AppCompatActivity {
     }
 
     private void setListView(){
-//        getRequests();
-
         ArrayList<String> distanceList = new ArrayList<>();
-        for(ParseObject request : requestsList){
-           distanceList.add(getRequestDistance());
+        if (requestsList != null && requestsList.size() > 0) {
+            for(ParseObject request : requestsList){
+                distanceList.add(getRequestDistance(request));
+            }
+        } else {
+            distanceList.add("No New Requests at this time");
         }
 
         ArrayAdapter adapter = new ArrayAdapter(getApplicationContext(),android.R.layout.simple_list_item_1, distanceList);
@@ -89,8 +122,13 @@ public class DriverActivity extends AppCompatActivity {
 
     }
 
-    private String getRequestDistance(){
-        return "chreast distance";
+    private String getRequestDistance(ParseObject request){
+        ParseGeoPoint requestLocation = (ParseGeoPoint) request.get("riderLocation");
+        float[] distance = new float[1];
+        Location.distanceBetween(userLocation.getLatitude(), userLocation.getLongitude(), requestLocation.getLatitude(), requestLocation.getLongitude(), distance);
+        Log.i("Distance", String.valueOf(distance[0] * 0.000621371192f));
+
+        return distance[0] * 0.000621371192f + " miles away";
     }
 
     private void getRequests(){
@@ -101,8 +139,11 @@ public class DriverActivity extends AppCompatActivity {
                 if (e == null) {
                     requestsList = new ArrayList();
                     requestsList.addAll(objects);
+                } else {
+                    if(requestsList == null){
+                        requestsList = new ArrayList<>();
+                    }
                 }
-                setListView();
             }
         });
 
