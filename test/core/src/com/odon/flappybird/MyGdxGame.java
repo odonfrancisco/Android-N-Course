89d6afd2a48c7429d7246dc50dcc4703d161db89
odonfrancisco <francisco.odon11@gmail.com>
2019-08-23 12:23:11 -0400

    Added movement to flappy bird on touch. Also have pipes showing up, next step will be to make the pipes move to the left

--- test/core/src/com/odon/flappybird/MyGdxGame.java
+++ test/core/src/com/odon/flappybird/MyGdxGame.java
@@ -6,12 +6,23 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+import sun.rmi.runtime.Log;
+
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture background;
 
 	Texture[] birds;
 	int flapState = 0;
+	float birdY = 0;
+	float velocity = 0;
+
+	Texture[] pipes;
+	float topPipeY;
+	float pipeX;
+
+	int gameState = 0;
+	float gravity = 3/2;
 
 	@Override
 	public void create () {
@@ -20,10 +31,32 @@ public class MyGdxGame extends ApplicationAdapter {
 		birds = new Texture[2];
         birds[0] = new Texture("bird.png");
         birds[1] = new Texture("bird2.png");
+		birdY = Gdx.graphics.getHeight()/2 - birds[flapState].getHeight()/2;
+		pipes = new Texture[2];
+		pipes[0] = new Texture("toptube.png");
+		pipes[1] = new Texture("bottomtube.png");
+		pipeX = Gdx.graphics.getWidth() - pipes[0].getWidth();
+		topPipeY = Gdx.graphics.getHeight() - 250;
 	}
 
 	@Override
 	public void render () {
+		if (gameState != 0) {
+			if(Gdx.input.justTouched()){
+				velocity = -30;
+			}
+
+			if (birdY > 0 || velocity < 0) {
+				velocity += gravity;
+				birdY -= velocity;
+			}
+
+		} else {
+			if(Gdx.input.justTouched()){
+				gameState = 1;
+			}
+		}
+
 		if (flapState == 0) {
 			flapState = 1;
 		} else {
@@ -33,9 +66,10 @@ public class MyGdxGame extends ApplicationAdapter {
 		batch.begin();
 		batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		float birdx = Gdx.graphics.getWidth()/2 - birds[flapState].getWidth()/2;
-		float birdy = Gdx.graphics.getHeight()/2 - birds[flapState].getHeight()/2;
-        batch.draw(birds[flapState], birdx, birdy);
-        batch.end();
+		batch.draw(birds[flapState], birdx, birdY);
+		batch.draw(pipes[0], pipeX, topPipeY);
+		batch.draw(pipes[1], pipeX, topPipeY - 400 - pipes[1].getHeight());
+		batch.end();
 	}
 	
 	@Override
