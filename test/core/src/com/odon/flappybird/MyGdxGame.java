a0c28d20fb15b40a05061ef40e613b27a0b635a0
odonfrancisco <francisco.odon11@gmail.com>
2019-08-26 15:41:52 -0400

    pipes automatically move to the left and continue doing so forever. Will most likely be adding collision next. Rob does a good job of teaching how to code android but he doesn't delve into OOP at all which i think would be useful for this kind of application

--- test/core/src/com/odon/flappybird/MyGdxGame.java
+++ test/core/src/com/odon/flappybird/MyGdxGame.java
@@ -6,6 +6,8 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+import java.util.Random;
+
 import sun.rmi.runtime.Log;
 
 public class MyGdxGame extends ApplicationAdapter {
@@ -19,10 +21,19 @@ public class MyGdxGame extends ApplicationAdapter {
 
 	Texture[] pipes;
 	float topPipeY;
-	float pipeX;
+	int numberOfPipes = 4;
+	float[] pipeX = new float[numberOfPipes];
+	float[] tubeOffset = new float[numberOfPipes];
 
 	int gameState = 0;
 	float gravity = 3/2;
+	float pipeGap = 400;
+	float maxTubeOffset;
+	Random randomGenerator;
+	float tubeVelocity = 4;
+
+	float distanceBetweenTubes;
+
 
 	@Override
 	public void create () {
@@ -32,19 +43,44 @@ public class MyGdxGame extends ApplicationAdapter {
         birds[0] = new Texture("bird.png");
         birds[1] = new Texture("bird2.png");
 		birdY = Gdx.graphics.getHeight()/2 - birds[flapState].getHeight()/2;
+
 		pipes = new Texture[2];
 		pipes[0] = new Texture("toptube.png");
 		pipes[1] = new Texture("bottomtube.png");
-		pipeX = Gdx.graphics.getWidth() - pipes[0].getWidth();
 		topPipeY = Gdx.graphics.getHeight() - 250;
+
+		maxTubeOffset = Gdx.graphics.getHeight() / 2 - pipeGap - 100;
+		randomGenerator = new Random();
+		distanceBetweenTubes = Gdx.graphics.getWidth() * 3/4;
+
+		for (int i = 0; i < numberOfPipes; i ++){
+			tubeOffset[i] = (randomGenerator.nextFloat() - 0.5f) * (Gdx.graphics.getHeight() - pipeGap - 200);
+			pipeX[i] = Gdx.graphics.getWidth() - pipes[1].getWidth() + i * distanceBetweenTubes ;
+
+		}
 	}
 
 	@Override
 	public void render () {
+		batch.begin();
+		batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
 		if (gameState != 0) {
+
 			if(Gdx.input.justTouched()){
 				velocity = -30;
 			}
+
+			for (int i = 0; i < numberOfPipes; i++ ){
+				if(pipeX[i] < - pipes[0].getWidth()){
+					pipeX[i] += numberOfPipes * distanceBetweenTubes;
+				} else {
+					pipeX[i] = pipeX[i] - tubeVelocity;
+					batch.draw(pipes[0], pipeX[i], topPipeY + tubeOffset[i]);
+					batch.draw(pipes[1], pipeX[i], topPipeY - pipeGap - pipes[1].getHeight() + tubeOffset[i]);
+				}
+			}
+
 
 			if (birdY > 0 || velocity < 0) {
 				velocity += gravity;
@@ -63,12 +99,9 @@ public class MyGdxGame extends ApplicationAdapter {
 			flapState = 0;
 		}
 
-		batch.begin();
-		batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
 		float birdx = Gdx.graphics.getWidth()/2 - birds[flapState].getWidth()/2;
 		batch.draw(birds[flapState], birdx, birdY);
-		batch.draw(pipes[0], pipeX, topPipeY);
-		batch.draw(pipes[1], pipeX, topPipeY - 400 - pipes[1].getHeight());
 		batch.end();
 	}
 	
