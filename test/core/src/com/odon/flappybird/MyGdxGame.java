d918004bc95cb8d0dc4e62a14590abfd67cc5b5c
odonfrancisco <francisco.odon11@gmail.com>
2019-08-28 20:20:54 -0400

    collision dection working

--- test/core/src/com/odon/flappybird/MyGdxGame.java
+++ test/core/src/com/odon/flappybird/MyGdxGame.java
@@ -8,6 +8,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Circle;
+import com.badlogic.gdx.math.Intersector;
 import com.badlogic.gdx.math.Rectangle;
 
 import java.util.Random;
@@ -15,31 +16,32 @@ import java.util.Random;
 import sun.rmi.runtime.Log;
 
 public class MyGdxGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture background;
-	ShapeRenderer shapeRenderer;
+	private SpriteBatch batch;
+	private Texture background;
+	private ShapeRenderer shapeRenderer;
 
-	Texture[] birds;
-	int flapState = 0;
-	float birdY = 0;
-	float velocity = 0;
-	Circle birdCircle = new Circle();
+	private Texture[] birds;
+	private int flapState = 0;
+	private float birdY = 0;
+	private float velocity = 0;
+	private Circle birdCircle = new Circle();
 
-	Texture[] pipes;
-	float topPipeY;
-	int numberOfPipes = 4;
-	float[] pipeX = new float[numberOfPipes];
-	float[] tubeOffset = new float[numberOfPipes];
-	Rectangle[] pipeRectangles = new Rectangle[numberOfPipes];
+	private Texture[] pipes;
+	private float topPipeY;
+	private int numberOfPipes = 4;
+	private float[] pipeX = new float[numberOfPipes];
+	private float[] tubeOffset = new float[numberOfPipes];
+	private Rectangle[] bottomPipeRectangles = new Rectangle[numberOfPipes];
+	private Rectangle[] topPipeRectangles = new Rectangle[numberOfPipes];
 
-	int gameState = 0;
-	float gravity = 3/2;
-	float pipeGap = 400;
-	float maxTubeOffset;
-	Random randomGenerator;
-	float tubeVelocity = 4;
+	private int gameState = 0;
+	private float gravity = 3/2;
+	private float pipeGap = 400;
+	private float maxTubeOffset;
+	private Random randomGenerator;
+	private float tubeVelocity = 4;
 
-	float distanceBetweenTubes;
+	private float distanceBetweenTubes;
 
 
 	@Override
@@ -65,7 +67,8 @@ public class MyGdxGame extends ApplicationAdapter {
 		for (int i = 0; i < numberOfPipes; i ++){
 			tubeOffset[i] = (randomGenerator.nextFloat() - 0.5f) * (Gdx.graphics.getHeight() - pipeGap - 200);
 			pipeX[i] = Gdx.graphics.getWidth() - pipes[1].getWidth() + i * distanceBetweenTubes ;
-
+			topPipeRectangles[i] = new Rectangle();
+			bottomPipeRectangles[i] = new Rectangle();
 		}
 	}
 
@@ -73,8 +76,11 @@ public class MyGdxGame extends ApplicationAdapter {
 	public void render () {
 		batch.begin();
 		batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
-		shapeRenderer.setColor(Color.RED);
+//		shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+//		shapeRenderer.setColor(Color.RED);
+
+		birdCircle.set(Gdx.graphics.getWidth()/2, birdY + birds[flapState].getHeight() / 2, birds[flapState].getWidth() / 2);
+//		shapeRenderer.circle(birdCircle.x, birdCircle.y, birdCircle.radius);
 
 		if (gameState != 0) {
 
@@ -88,13 +94,25 @@ public class MyGdxGame extends ApplicationAdapter {
 					tubeOffset[i] = (randomGenerator.nextFloat() - 0.5f) * (Gdx.graphics.getHeight() - pipeGap - 200);
 				} else {
 					pipeX[i] = pipeX[i] - tubeVelocity;
-					batch.draw(pipes[0], pipeX[i], topPipeY + tubeOffset[i]);
-					batch.draw(pipes[1], pipeX[i], topPipeY - pipeGap - pipes[1].getHeight() + tubeOffset[i]);
+				}
+				batch.draw(pipes[0], pipeX[i], topPipeY + tubeOffset[i]);
+				batch.draw(pipes[1], pipeX[i], topPipeY - pipeGap - pipes[1].getHeight() + tubeOffset[i]);
 //					shapeRenderer.set(ShapeRenderer.ShapeType.Filled);
 //					shapeRenderer.setColor(Color.MAGENTA);
-					shapeRenderer.rect(pipeX[i], topPipeY + tubeOffset[i], pipes[0].getWidth(), pipes[0].getHeight());
-					shapeRenderer.rect(pipeX[i], topPipeY - pipeGap - pipes[1].getHeight() + tubeOffset[i], pipes[1].getWidth(), pipes[0].getHeight());
+
+				topPipeRectangles[i] = new Rectangle(pipeX[i], topPipeY + tubeOffset[i],
+						pipes[0].getWidth(), pipes[0].getHeight());
+				bottomPipeRectangles[i] = new Rectangle(pipeX[i], topPipeY - pipeGap - pipes[1].getHeight() + tubeOffset[i],
+						pipes[1].getWidth(), pipes[1].getHeight());
+
+//				shapeRenderer.rect(topPipeRectangles[i].x, topPipeRectangles[i].y,
+//						pipes[0].getWidth(), pipes[0].getHeight());
+//				shapeRenderer.rect(bottomPipeRectangles[i].x, bottomPipeRectangles[i].y,
+//						pipes[1].getWidth(), pipes[0].getHeight());
 //					shapeRenderer.end();
+
+				if(Intersector.overlaps(birdCircle, topPipeRectangles[i]) || Intersector.overlaps(birdCircle, bottomPipeRectangles[i])){
+					Gdx.app.log("Collision", "Collision truth");
 				}
 			}
 
@@ -121,9 +139,7 @@ public class MyGdxGame extends ApplicationAdapter {
 		batch.draw(birds[flapState], birdx, birdY);
 		batch.end();
 
-		birdCircle.set(Gdx.graphics.getWidth()/2, birdY + birds[flapState].getHeight() / 2, birds[flapState].getWidth() / 2);
 
-		shapeRenderer.circle(birdCircle.x, birdCircle.y, birdCircle.radius);
 		shapeRenderer.end();
 	}
 	
