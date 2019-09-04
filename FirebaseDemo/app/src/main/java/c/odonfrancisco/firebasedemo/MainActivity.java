dd90ff6db74b81920c2a0dc738d9891e060f262d
odonfrancisco <francisco.odon11@gmail.com>
2019-09-04 08:58:08 -0400

    added a banner ad at the bottom of the screen. simple intro to introducing ads into an app

--- FirebaseDemo/app/src/main/java/c/odonfrancisco/firebasedemo/MainActivity.java
+++ FirebaseDemo/app/src/main/java/c/odonfrancisco/firebasedemo/MainActivity.java
@@ -7,6 +7,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -21,23 +24,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        DatabaseReference dbref = FirebaseDatabase.getInstance().getReference();
+        MobileAds.initialize(this, String.valueOf(R.string.admobAppID));
 
-        Map<String, String> values = new HashMap<>();
+//        DatabaseReference dbref = FirebaseDatabase.getInstance().getReference();
+//
+//        Map<String, String> values = new HashMap<>();
+//
+//        values.put("name", "odon");
+//
+//        dbref.push().setValue(values, new DatabaseReference.CompletionListener() {
+//            @Override
+//            public void onComplete(@Nullable DatabaseError databaseError, @NonNull DatabaseReference databaseReference) {
+//                if (databaseError == null) {
+//                    Log.i("Info", "Save successful");
+//                } else {
+//                    Log.i("DBError", databaseError.getDetails());
+//                    Log.i("Info", "Save Failed");
+//                }
+//            }
+//        });
 
-        values.put("name", "odon");
-
-        dbref.push().setValue(values, new DatabaseReference.CompletionListener() {
-            @Override
-            public void onComplete(@Nullable DatabaseError databaseError, @NonNull DatabaseReference databaseReference) {
-                if (databaseError == null) {
-                    Log.i("Info", "Save successful");
-                } else {
-                    Log.i("DBError", databaseError.getDetails());
-                    Log.i("Info", "Save Failed");
-                }
-            }
-        });
+        AdView adView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        adView.loadAd(adRequest);
 
     }
 }
